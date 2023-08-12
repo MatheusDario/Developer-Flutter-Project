328d6e2440dbfb99032159cd4262aeedc23af6ba
Matheus Dario <dario.matheus97@gmail.com>
2023-08-12 17:51:41 -0300

    first commit

---
+++ project/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black45,
+        title: Text('I am Developer!'),
+        centerTitle: true,
+      ),
+    ),
+  ));
+}
