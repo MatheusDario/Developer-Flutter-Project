2ae81518ce86213067aa1cf867a7b16a5c68aa50
Matheus Dario <dario.matheus97@gmail.com>
2023-08-12 23:46:52 -0300

    Finished project

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.grey[800],
         title: const Text('I am Developer!'),
         centerTitle: true,
       ),
-      backgroundColor: Colors.grey[800],
+      backgroundColor: Colors.grey[500],
       body: const Center(
           child: Image(
             image: AssetImage('images/developer.jpg'),
